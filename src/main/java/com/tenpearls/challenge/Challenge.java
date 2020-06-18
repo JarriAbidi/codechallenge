d362da7a127b383c99e12a57d9fb79cb4189416e
Jarri Abidi <jarri1337@gmail.com>
2020-06-18 21:41:31 +0500

    Rename variable for clarity

--- src/main/java/com/tenpearls/challenge/Challenge.java
+++ src/main/java/com/tenpearls/challenge/Challenge.java
@@ -10,10 +10,10 @@ public class Challenge {
 
     public static Integer[] sortOddBeforeEven(Integer[] input) {
         Deque<Integer> result = new ArrayDeque<>();
-        int l = input.length;
-        for (int i = 0; i < l; i++) {
-            if (input[l - 1 - i] % 2 != 0) {
-                result.addFirst(input[l - 1 - i]);
+        int last = input.length - 1;
+        for (int i = 0; i <= last; i++) {
+            if (input[last - i] % 2 != 0) {
+                result.addFirst(input[last - i]);
             }
             if (input[i] % 2 == 0) {
                 result.addLast(input[i]);
