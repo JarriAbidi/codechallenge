4edd7afa1b53dc2b63585642b603f25d408afe2d
Jarri Abidi <jarri1337@gmail.com>
2020-06-18 20:22:30 +0500

    Initial commit

---
+++ src/main/java/com/tenpearls/challenge/Challenge.java
@@ -0,0 +1,23 @@
+package com.tenpearls.challenge;
+
+import java.util.*;
+
+public class Challenge {
+
+    public static void main(String[] args) {
+    }
+
+    public static Integer[] sortOddBeforeEven(Integer[] input) {
+        Queue<Integer> odd = new ArrayDeque<>();
+        Queue<Integer> even = new ArrayDeque<>();
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] % 2 != 0) {
+                odd.add(input[i]);
+            } else {
+                even.add(input[i]);
+            }
+        }
+        odd.addAll(even);
+        return odd.toArray(new Integer[odd.size()]);
+    }
+}
